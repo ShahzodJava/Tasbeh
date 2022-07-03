778ffc6d6d23b470a419788d1a98ba4792fb09e0
Shahzod-Ashurmatov <75681220+ShahzodJava@users.noreply.github.com>
2022-07-03 19:21:54 +0500

    added toolbar and created setting activity

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,6 +27,9 @@ public class MainActivity<Int> extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
     private Vibrator vibrator;
     private Sizes sizeName;
+    private int time;
+    private TextView textViewTime;
+    private ImageView settingImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +40,36 @@ public class MainActivity<Int> extends AppCompatActivity {
         textViewCount = findViewById(R.id.textViewCount);
         buttonReset = findViewById(R.id.buttonReset);
         spinner = findViewById(R.id.spinnerCounter);
-        vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
-        sizeName = Sizes.SMALL;
+        textViewTime = findViewById(R.id.textViewTime);
+
+
+        sizeName = Sizes.TINY;
         countLimit = sizeName.getSize();
 
         count = sizeName.getCount();
+        time = sizeName.getTime();
+
         mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.glass);
+        vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
         imageViewTasbih.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 count++;
+                if (count>countLimit) {
+
+                    count = 0;
+                    time++;
+                    sizeName.setTime(time);
+                    textViewTime.setText("x"+sizeName.getTime());
+
+
+                }
+
                 textViewCount.setText(Integer.toString(count));
                 sizeName.setCount(count);
-
-                if (count==countLimit) count = 0;
 
                 try {
                     mediaPlayer.stop();
@@ -86,8 +105,13 @@ public class MainActivity<Int> extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 count = 0;
+                time = 0;
+
                 sizeName.setCount(count);
-                textViewCount.setText(""+sizeName.getCount() );
+                sizeName.setTime(time);
+
+                textViewCount.setText(""+sizeName.getCount());
+                textViewTime.setText("x"+sizeName.getTime());
             }
         });
 //
@@ -95,15 +119,20 @@ public class MainActivity<Int> extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String value = adapterView.getItemAtPosition(i).toString();
+                Log.v("Main Activity","view: " + value);
+                System.out.println(value);
                 countLimit = Integer.parseInt(value);
                 sizeName = Sizes.findBySize(countLimit);
+
                 count = sizeName.getCount();
+                time = sizeName.getTime();
+
                 textViewCount.setText(Integer.toString(count));
+                textViewTime.setText("x"+time);
             }
 
             @Override
@@ -112,32 +141,55 @@ public class MainActivity<Int> extends AppCompatActivity {
             }
 
         });
+
+        settingImageView = findViewById(R.id.settings);
+        settingImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
 
 enum Sizes{
-    SMALL(33, 0),
-    MEDIUM(99, 0),
-    BIG(1000, 0),
-    LARGE(70000, 0);
+    TINY(11,0,0),
+    SMALL(33, 0,0),
+    MEDIUM(99, 0,0),
+    BIG(1000, 0,0),
+    LARGE(70000, 0,0);
 
 
     private int size;
     private int count;
-    Sizes(int size, int count) {
+    private int time;
+
+    Sizes(int size, int count, int time) {
         this.size = size;
         this.count = count;
+        this.time = time;
     }
 
     public int getSize() {
         return this.size;
     }
+
     public int getCount() {
         return this.count;
     }
     public void setCount(int count) {
         this.count = count;
     }
+
+    public int getTime() {
+        return this.time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
     public static Sizes findBySize(int size){
         for (Sizes value:values()) {
             if (value.size == size) {
@@ -146,10 +198,9 @@ enum Sizes{
         }
         return null;
     }
+
     @Override
     public String toString(){
         return ""+this.getSize();
     }
-
-
 }
